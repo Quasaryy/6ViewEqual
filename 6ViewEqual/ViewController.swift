a33413d49e39991a161504c50977ed458e3ad470
Yury <quasaryy@gmail.com>
2022-12-28 22:13:40 +0400

    Initial Commit

---
+++ 6ViewEqual/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6ViewEqual
+//
+//  Created by Yury on 28.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
